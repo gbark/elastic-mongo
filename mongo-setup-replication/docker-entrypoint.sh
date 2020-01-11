1e417cb2ca1e518866f7b4981e6141e5cd9cc1a2
Gustaf Barkeling <gustaf@frontengineering.com>
2020-01-11 10:10:20 +0000

    Setup es to run as a cluster and add kibana and mongo-express for browsing data

--- mongo-setup-replication/docker-entrypoint.sh
+++ mongo-setup-replication/docker-entrypoint.sh
@@ -16,8 +16,9 @@ mongo --host "mongodb0" <<EOF
    rs.initiate({
     _id : "rs0",
     members: [
-        { _id: 0, host: "mongodb0:27017" },
-        { _id: 1, host: "mongodb1:27017" }
+        { _id: 0, host: "mongodb0:27017", priority: 2 },
+        { _id: 1, host: "mongodb1:27017" },
+        { _id: 2, host: "mongodb2:27017" }
     ]
   }, { force: true })
 EOF
